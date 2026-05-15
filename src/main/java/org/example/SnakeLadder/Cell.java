4717a4aa64d5e8425f2adad7e9f7962b306994e6
Pavesh Kanungo <paveshkanungo@gmail.com>
2026-05-15 12:57:46 +0530

    Update SnakeLadder

---
+++ src/main/java/org/example/SnakeLadder/Cell.java
@@ -0,0 +1,13 @@
+package org.example.SnakeLadder;
+
+public class Cell {
+    Jump jump;
+
+    public Jump getJump() {
+        return jump;
+    }
+
+    public void setJump(Jump jump) {
+        this.jump = jump;
+    }
+}
---
+++ src/main/java/org/example/SnakeLadder/Main.java
@@ -0,0 +1,8 @@
+package org.example.SnakeLadder;
+
+public class Main {
+    public static void main(String args[]) {
+        Game obj = new Game();
+        obj.startGame();
+    }
+}
